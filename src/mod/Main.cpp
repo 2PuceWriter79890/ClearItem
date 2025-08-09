956d9e3e59588b9f6306ba3e43104df73a9cfa30
L QJ <96654617+2PuceWriter79890@users.noreply.github.com>
2025-08-10 07:41:01 +0800

    Update Main.cpp

--- src/mod/Main.cpp
+++ src/mod/Main.cpp
@@ -1,28 +1,28 @@
 #include "mod/InvCleaner.h"
-#include <ll/api/plugin/RegisterHelper.h>
 
-LL_PLUGIN_ENTRY_HOOK(
+#include <ll/api/mod/RegisterHelper.h>
+LL_MOD_ENTRY_HOOK(
     inv_cleaner_main,
-    ll::plugin::NativePlugin&,
-    ll::plugin::PluginLoadReason reason
+    ll::mod::NativeMod&,
+    ll::mod::ModLoadReason reason
 ) {
-    if (reason == ll::plugin::PluginLoadReason::Load) {
+    if (reason == ll::mod::ModLoadReason::Load) {
         inv_cleaner::InvCleanerMod::getInstance().load();
     }
     return true;
 }
 
-LL_PLUGIN_ENABLE_HOOK(inv_cleaner_main, ll::plugin::NativePlugin&) {
+LL_MOD_ENABLE_HOOK(inv_cleaner_main, ll::mod::NativeMod&) {
     inv_cleaner::InvCleanerMod::getInstance().enable();
     return true;
 }
 
-LL_PLUGIN_DISABLE_HOOK(inv_cleaner_main, ll::plugin::NativePlugin&) {
+LL_MOD_DISABLE_HOOK(inv_cleaner_main, ll::mod::NativeMod&) {
     inv_cleaner::InvCleanerMod::getInstance().disable();
     return true;
 }
 
-LL_REGISTER_PLUGIN(
+LL_REGISTER_MOD(
     "InvCleaner",
     "Asynchronously cleans items from all players.",
     "1.0.0",
